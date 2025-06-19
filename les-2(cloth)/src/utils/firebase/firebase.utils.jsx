20edc4de9837543a9341634a50254699235110b6
sonya-cmd <sofiyabudanova7@gmail.com>
2025-06-19 23:09:44 +0300

    added our sign in component as well as firebase utils and brought in firebase libraries

--- les-2(cloth)/src/utils/firebase/firebase.utils.jsx
+++ les-2(cloth)/src/utils/firebase/firebase.utils.jsx
@@ -6,6 +6,14 @@ import {
     GoogleAuthProvider 
 } from 'firebase/auth';
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyATy7bbUMDpW_dhvMKSLbn55anC5vIyOPY",
     authDomain: "crwn-clothinh-db-87498.firebaseapp.com",
@@ -25,3 +33,33 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+
+  const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid );
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+    
+    if(!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createAt = new Date();
+
+        try {
+            await setDoc(userDocReef, {
+                displayName,
+                email, 
+                createAt
+            });
+        }catch (error) {
+            console.log('error creating the user', error.message);
+
+        }
+    }
+
+    return userDocRef;
+  };
