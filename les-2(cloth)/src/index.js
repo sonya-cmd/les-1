8fb36b1f924a2b36b24866a8f5c54eb4c22db34a
sonya-cmd <sofiyabudanova7@gmail.com>
2025-06-23 23:03:49 +0300

    Complete React Context setup for state management

--- les-2(cloth)/src/index.js
+++ les-2(cloth)/src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // обязательно .client
 import { BrowserRouter } from 'react-router-dom';
+
 import './index.scss';
+
 import App from './App';
+import { UserProvider } from './contexts/user.context';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <UserProvider>
       <App />
+    </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
