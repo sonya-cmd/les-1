71b513cddaceaf0bbfded8a4762edd6685cf70b8
sonya-cmd <sofiyabudanova7@gmail.com>
2025-06-29 23:01:29 +0300

    Add ProductCard component with styling and button integration

--- les-2(cloth)/src/App.js
+++ les-2(cloth)/src/App.js
@@ -3,14 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/home/navigation/navigation.component';
 import Authentication from './routes/home/authentication/authentication.component';
-
-
-
-const Shop = () => {
- 
-    <h1>I am the shop page</h1>;
-  
-};
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return ( 
--- les-2(cloth)/src/index.js
+++ les-2(cloth)/src/index.js
@@ -6,6 +6,7 @@ import './index.scss';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductProvider } from './contexts/products.context';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -14,7 +15,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
     <UserProvider>
-      <App />
+      <ProductsProvider>
+        <App />
+      </ProductsProvider>
     </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
