6cba79b683d67d7efafb4609ca18ffbe4e36ec44
sonya-cmd <sofiyabudanova7@gmail.com>
2025-07-01 23:50:50 +0300

    added in our addToCard feature

--- les-2(cloth)/src/routes/home/navigation/navigation.component.jsx
+++ les-2(cloth)/src/routes/home/navigation/navigation.component.jsx
@@ -2,7 +2,7 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import CartIcon from '../../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../../components/card-dropdown/card-dropdown.component';
+import CartDropdown from '../../../components/cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg';
 
